76519a7dadbe30a15edab934f3ea271778cd7f3d
github username <github email address>
2019-09-08 20:35:50 -0400

    console.log store name and price

--- src/CompareCard.js
+++ src/CompareCard.js
@@ -2,31 +2,41 @@ import React from 'react'
 const priceYugeAPI_KEY = 'fCBxRI3EUVk2kSMxPLkGYTcXpvPRfx1XN4C';
 
  class CompareCard extends React.Component {
-     state = {
-       product_compare: []
-   };
+   constructor() {
+   super()
+   this.state = { product_compare: [] }
+ }
 
    componentDidMount(){
+     let product_compare = [];
      fetch('https://price-api.datayuge.com/api/v1/compare/detail?' + `api_key=${priceYugeAPI_KEY}&id=${this.props.product_id}`)
        .then(response => response.json())
          // console.log(response.json())
-       .then(jsonResp => this.setState({product_compare: jsonResp.data.stores}));//
-        console.log('stores', this.state.product_compare)
-       //set state to jsoResp
+       .then(jsonResp => {console.log('jsonResp', jsonResp);
+        this.setState({product_compare: jsonResp.data.stores})
+      });
+
+     }
+
+     retrieveStoreInfo(array){
+       {Object.values(this.state.product_compare).map((storeObj, index) => {
+           for (let key of Object.keys(storeObj)) {
+               let store = storeObj[key];
+ // ... do something with mealName
+          console.log('store name', store.product_store, 'price', store.product_price);
+           }
+            // return <p> {console.log('prices', Object.values(store))} </p>
+          })}
      }
 
   render() {
-         return(
-           <div>
-             Product Compare
-             {this.state.proudct_compare.map((store, index) => (
-        <p> {console.log({index})}!</p>
-    ))}
-           </div>
-         )
-     }
- }
-
+    return (
+      <div>
+          {this.retrieveStoreInfo(this.state.product_compare)}
+      </div>
+    )
+  }
+}
 
 
  export default CompareCard
