501a1cd7911c5d9fbd9eb8ede849298e4283cab4
github username <github email address>
2019-09-21 22:41:16 -0400

    added thunk - top search action

--- src/actions/fetchTopSearches.js
+++ src/actions/fetchTopSearches.js
@@ -6,3 +6,11 @@
 //       .then(items => dispatch({ type: 'SEARCH_ITEMS', cats }));
 //   };
 // }
+export function fetchTopSearches() {
+  return (dispatch) => {
+    dispatch({ type: 'FETCH_TOP_SEARCHES' });
+    return fetch('http://api/v1/top_searches)
+      .then(response => response.json())
+      .then(top_searches => dispatch({ type: 'FETCH_TOP_SEARCHES', top_searches }));
+  };
+}
